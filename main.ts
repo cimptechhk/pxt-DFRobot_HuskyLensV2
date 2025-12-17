5767ad2adeacf30eba3452d68e68d75f00cc2f86
CIMP Tech <99329136+cimptechhk@users.noreply.github.com>
2025-12-18 01:03:48 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,6 +13,9 @@ smarthonIoTBit.onThingspeakConn(function (status, errorCode) {
 input.onButtonPressed(Button.B, function () {
     SmartCity.turn_servo(180, AnalogPin.P1)
 })
+let CenterLicenseNo = 0
+let CenterLicenseID = 0
+let TotalLicenses = 0
 OLED.init(128, 64)
 smarthonIoTBit.initializeWifi(SerialPin.P16, SerialPin.P8)
 smarthonIoTBit.setWifi("FWIOT24", "Y@nShing3")
@@ -21,5 +24,31 @@ huskylensV2.switchAlgorithm(huskylensV2.Algorithm.ALGORITHM_LICENSE_RECOGNITION)
 basic.forever(function () {
     if (smarthonIoTBit.isWifiConnect()) {
         huskylensV2.getResultPlateRecogtion()
+        TotalLicenses = huskylensV2.getCachedResultNumPlate()
+        CenterLicenseID = huskylensV2.getCachedCenterPlateResult(huskylensV2.PlateProperty.ID)
+        CenterLicenseNo = huskylensV2.getCachedCenterPlateResult(huskylensV2.PlateProperty.Content)
+        if (huskylensV2.availablePlateRecogtion()) {
+            OLED.clear()
+            basic.showNumber(TotalLicenses)
+            OLED.writeStringNewLine("Total Licenses: " + TotalLicenses)
+            OLED.writeStringNewLine("Center License ID: " + CenterLicenseID)
+            OLED.writeStringNewLine("Center License No: " + CenterLicenseNo)
+            if (CenterLicenseID == -1 || CenterLicenseID == 0) {
+                OLED.writeStringNewLine("Gate Status: " + "Access Denied")
+                SmartCity.turn_servo(180, AnalogPin.P1)
+            } else {
+                OLED.writeStringNewLine("Gate Status: " + "Opening...")
+                music.play(music.tonePlayable(262, music.beat(BeatFraction.Breve)), music.PlaybackMode.InBackground)
+                SmartCity.turn_servo(90, AnalogPin.P1)
+                basic.pause(3000)
+                SmartCity.turn_servo(180, AnalogPin.P1)
+            }
+            smarthonIoTBit.sendThingspeak(
+            "WRQN2R7OMIMHQVDL",
+            TotalLicenses,
+            CenterLicenseID
+            )
+            basic.pause(12000)
+        }
     }
 })
